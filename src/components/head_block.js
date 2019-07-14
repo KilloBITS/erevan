4542f0f5806742ba7db8519ca03de501f665bee6
mr.kalinuk <qazwsx159357>
2019-07-14 21:50:08 +0300

    5

--- src/components/head_block.js
+++ src/components/head_block.js
@@ -41,9 +41,7 @@ class HeadBlock extends React.Component {
 
   render() {
     return <div className="block head" id="HeadBlock">
-
       <div className="staticMenuHead">{parseMenu(this.props.menu, true)}</div>
-
       <div className="background_block">
         <div className={(this.props.bigMainLogo)?"mainLogo":"mainLogo hide"}>
           <ReactVivus
@@ -60,15 +58,12 @@ class HeadBlock extends React.Component {
         <div className={(this.props.bigMainLogo)?"miniInfoBlock show":"miniInfoBlock"}>
           {parseMiniInfo(this.props.data.miniInfo)}
         </div>
-
         <div className={(this.props.bigMainLogo)?"headerFood bgImg_1 show":"headerFood bgImg_1"}>
           <img src={this.props.data.background_1} alt="Єреван"/>
           <div className="headFoodLink">Оджахурі на кеци</div>
         </div>
       </div>
-
       <div className="blockToScroll"></div>
-
       <div className={(this.props.top > 0)?"bottomBorder isScrolled":"bottomBorder"}>
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         	 viewBox="0 0 1440 129" style={{enableBackground:"new 0 0 1440 129"}}>
@@ -76,7 +71,6 @@ class HeadBlock extends React.Component {
         	C980.9,160.6,956.5-85.5,293.5,78z"/>
         </svg>
       </div>
-
     </div>
   }
 }
